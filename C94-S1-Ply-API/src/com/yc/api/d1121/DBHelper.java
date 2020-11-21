018da2a0d2d43df7d9468e5e20e2b40202652208
Administrator <Administrator@516pc57>
2020-11-21 21:18:48 +0800

    JDBC封装

---
+++ C94-S1-Ply-API/src/com/yc/api/d1121/DBHelper.java
@@ -0,0 +1,133 @@
+package com.yc.api.d1121;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class DBHelper {
+
+	// OOP
+	static {
+		// 静态块 ： 类加载的执行一次, 块不允许抛出编译期异常
+		// 1.加载驱动
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+		} catch (ClassNotFoundException e) {
+			// 异常转型， 异常链
+			throw new RuntimeException("数据库驱动加载失败！", e);
+		}
+	}
+
+	public Connection getConnection() throws SQLException {
+		// 2.获取连接
+		String url = "jdbc:oracle:thin:@127.0.0.1:1521:orcl";
+		String user = "scott";
+		String password = "a";
+		return DriverManager.getConnection(url, user, password);
+	}
+
+	/**
+	 * 执行修改数据的SQL语句
+	 * 
+	 * @param sql
+	 * @param params
+	 *            可变参数数组 ， 这种参数必须写在方法参数的最后一个
+	 * @throws SQLException
+	 */
+	public int update(String sql, Object... params) throws SQLException {
+		Connection conn = getConnection();
+		try {
+			System.out.println("SQL: " + sql);
+			System.out.println("参数: " + Arrays.toString(params));
+			PreparedStatement ps = conn.prepareStatement(sql);
+			for (int i = 0; i < params.length; i++) {
+				ps.setObject(i + 1, params[i]);
+			}
+			return ps.executeUpdate();
+		} finally {
+			conn.close();
+		}
+	}
+
+	/**
+	 * List<Map<String,Object>> 有序可重复
+	 * 
+	 * 查询0~N行结果集
+	 * 
+	 * @param sql
+	 * @param params
+	 * @throws SQLException
+	 */
+	public List<Map<String, Object>> selectList(String sql, Object... params) throws SQLException {
+		Connection conn = getConnection();
+		try {
+			System.out.println("SQL: " + sql);
+			System.out.println("参数: " + Arrays.toString(params));
+			PreparedStatement ps = conn.prepareStatement(sql);
+			for (int i = 0; i < params.length; i++) {
+				ps.setObject(i + 1, params[i]);
+			}
+			List<Map<String, Object>> ret = new ArrayList<>();
+			ResultSet rs = ps.executeQuery();
+
+			// 结果集元数据（描述数据的数据）对象
+			ResultSetMetaData md = rs.getMetaData();
+			// md.getColumnCount(); // 获取结果集的列数
+			// md.getColumnName(column); // 获取指定序号的列名
+			while (rs.next()) {
+				Map<String, Object> row = new HashMap<String, Object>();
+				/**
+				 * 取出该行所有的列值，存放 row 集合中
+				 */
+				for (int i = 0; i < md.getColumnCount(); i++) {
+					String columnName = md.getColumnName(i + 1);
+					Object value = rs.getObject(i + 1);
+					row.put(columnName, value);
+				}
+				ret.add(row);
+			}
+			return ret;
+		} finally {
+			conn.close();
+		}
+	}
+
+	/**
+	 * 查询 select 语句结果集的行数
+	 * 
+	 * @param sql
+	 * @return
+	 */
+	public long count(String sql, Object... params) throws SQLException {
+		return 0;
+	}
+
+	/**
+	 * 查询 select 语句第一行第一个字段值（查询单值）
+	 * 
+	 * @param sql
+	 * @return
+	 */
+	public Object selectValue(String sql, Object... params) throws SQLException {
+		return null;
+	}
+
+	/**
+	 * 查询 select 语句唯一的结果（例如：根据主键值查询），如果返回大于1行数据，则抛出SQL异常
+	 * 
+	 * @param sql
+	 * @return
+	 */
+	public Map<String, Object> selectOne(String sql, Object... params) throws SQLException {
+		return null;
+	}
+
+}
---
+++ C94-S1-Ply-API/src/com/yc/api/d1121/DBHelperTest.java
@@ -0,0 +1,97 @@
+package com.yc.api.d1121;
+
+import java.sql.SQLException;
+import java.util.*;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class DBHelperTest {
+
+	@Test // 注解 ==》 给机器看的注释 Junit 是单元测试框架（白盒测试）
+	public void test() throws SQLException {
+		DBHelper dbh = new DBHelper();
+		String sql = "update dept set dname=? where deptno=?";
+		int ret = dbh.update(sql, "财务部", 10);
+		// 对结果的判断
+		Assert.assertEquals(1, ret); // 断言 : 不等于将会产生断言异常
+	}
+
+	@Test // 测试驱动开发
+	public void test1() throws SQLException {
+
+		String sql1 = "select * from emp";
+		DBHelper dbh = new DBHelper();
+		List<Map<String, Object>> list1 = dbh.selectList(sql1);
+		Assert.assertEquals(14, list1.size());
+
+		String sql2 = "select * from emp where deptno=10";
+		List<Map<String, Object>> list2 = dbh.selectList(sql2);
+		Assert.assertEquals(3, list2.size());
+		Assert.assertEquals("CLARK", list2.get(0).get("ENAME"));
+
+		System.out.println(list2);
+
+	}
+	
+	/**
+	 * 注意： 以下测试基于 SCOTT 用户的原始记录数据，如果有修改数据，可能会造成测试失败！
+	 * 
+	 * 		测试方法不允许修改代码
+	 */
+
+	/**
+	 * 测试查询结果集总数方法 ==》 子查询
+	 */
+	public void testCount() throws SQLException {
+		String sql = "select * from emp where deptno=?";
+		DBHelper dbh = new DBHelper();
+		long count = dbh.count(sql, 10);
+		Assert.assertEquals(3, count);
+	}
+
+	/**
+	 * 测试查询单值结果集方法
+	 */
+	public void testSelectValue() throws SQLException {
+		String sql = "select ename from emp where empno=?";
+		DBHelper dbh = new DBHelper();
+		Object ename = dbh.selectValue(sql, 7839);
+		Assert.assertEquals("KING", ename);
+
+		sql = "select job from emp where empno=?";
+		Object job = dbh.selectValue(sql, 7939);
+		Assert.assertEquals("PRESIDENT", job);
+
+		// 查询最大工资， 如果你没有改数据，最高工资是KING ， 5000元
+		sql = "select max(sal) maxsal from emp";
+		int maxsal = Integer.parseInt("" + dbh.selectValue(sql));
+		Assert.assertEquals(5000, maxsal);
+	}
+
+	/**
+	 * 测试查询单行数据结果集方法
+	 */
+	public void testSelectOne() throws SQLException {
+		// 根据主键查询 KING
+		String sql = "select * from emp where empno=?";
+		DBHelper dbh = new DBHelper();
+		Map<String, Object> row = dbh.selectOne(sql, 7839);
+		Assert.assertEquals("KING", row.get("ENAME"));
+
+		// 根据不存在的主键查询 null 结果
+		sql = "select * from emp where empno=?";
+		row = dbh.selectOne(sql, 9999);
+		Assert.assertNull(row); // 断言 row 为 null ， 没有编号为 9999 的员工
+
+		// 查询多行记录产生异常
+		try {
+			sql = "select * from emp where deptno=?";
+			row = dbh.selectOne(sql, 10);
+		} catch (SQLException e) { 
+			// throw new SQLException("结果集的行数大于1");
+			Assert.assertEquals("结果集的行数大于1", e.getMessage());
+		}
+	}
+
+}
